95fedb381bda0a53ea3e9f25ccb5d57d92be8e7f
David Kastelman <david.kastelman@gmail.com>
2014-07-25 17:41:14 -0700

    second commit

--- discount_calculator.py
+++ discount_calculator.py
@@ -1,6 +1,10 @@
 class DiscountCalculator(object):
-  def calculate(self, total, discount_amount, discount_type):
-    percentage_discount = float(discount_amount) / 100
-    discount = float(total) * percentage_discount
-    return discount
+	def calculate(self, total, discount_amount, discount_type):
+  		if discount_type == 'percent':
+  			percentage_discount = float(discount_amount) / 100
+  			discount = float(total) * percentage_discount
+  			return discount
+  		else:
+  			discount = discount_amount
+  			return discount
 
